ea240378c9f8e9276c83c2ff7ef5d156d6b0def6
roseayorkorjohnson <roseayorkorjohnson@gmail.com>
2026-07-21 09:10:15 -0700

    Added chamges

--- src/script.js
+++ src/script.js
@@ -5,3 +5,43 @@ const email = prompt("Please enter your email:");
 
 // 2. Alert the formatted message back to user
 alert (`Hi ${name}, you are ${age} years old and we'llkeep in touch via your email: ${email}`);
+
+// A. Store your name inside a variable and log it
+let myName = "Rose Johnson";
+console.log( "Name:", myName);
+
+// B. Store your age inside a variable and log it
+let myAge = 20;
+console.log("Age:", myAge);
+
+// C. Add two numbers
+let sum = 10 + 5;
+console.log("Sum:", sum); // output - 15
+
+// D. Subtract two numbers
+let difference = 20 - 7;
+console.log("Difference:",difference); // output - 13
+
+// E. Divide two numbers
+let quotient = 30 / 6;
+console.log("Quotient:",quotient); // output - 5
+
+// F. Multiply two numbers
+let product = 8 * 4;
+console.log("Product:", product); // output - 32
+
+// G. Find the remainder of 15 % 4
+let remainder = 15 % 4;
+console.log("Remainder:", remainder); // output 3
+
+// H. Store a boolean value
+let roseGold = true;
+console.log("Boolen:", roseGold);
+
+// I. Set value to a number and log type
+let roseValue = 100;
+console.log("Type:", typeof roseValue);  
+
+// J. Set value to a string and log type
+let stringValue = "Hello World";
+console.log("Type of stringvalue:", typeof stringValue);
