e64e749f03b7bae407de63451f98276d1a309631
roseayorkorjohnson <roseayorkorjohnson@gmail.com>
2026-07-21 07:00:56 -0700

    Added chamges

---
+++ src/script.js
@@ -0,0 +1,7 @@
+// 1. Prompt the user for name, age, and email
+const name = prompt("Please enter your name:");
+const age = prompt("Please enter your age:");
+const email = prompt("Please enter your email:");
+
+// 2. Alert the formatted message back to user
+alert (`Hi ${name}, you are ${age} years old and we'llkeep in touch via your email: ${email}`);
